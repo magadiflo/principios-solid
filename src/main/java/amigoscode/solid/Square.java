f40dc3e24e4c80ba1f2f63b008280e3de77a460d
Martín <magadiflo@gmail.com>
2022-12-12 09:08:52 -0500

    Codificando clases para ejemplificar principios SOLID

---
+++ src/main/java/amigoscode/solid/Square.java
@@ -0,0 +1,14 @@
+package amigoscode.solid;
+
+public class Square {
+
+    private final int length;
+
+    public Square(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
